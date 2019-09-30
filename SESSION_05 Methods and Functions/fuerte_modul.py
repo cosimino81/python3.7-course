36472cf03adf4a2cfa2d836717728ad2513ad96d
Cosimo Curiale <cosimo.curiale@gmail.com>
2019-09-30 10:11:21 +0200

    last commit

---
+++ SESSION_05 Methods and Functions/fuerte_modul.py
@@ -0,0 +1,15 @@
+
+# this is an example how to create a module
+def my_location():
+    '''
+    DOC: This function prints out my current location.
+    '''
+    print("I'm in Fuerteventura")
+
+def count_element(iterable):
+    '''
+    DOC: This function calculate the length of a list.
+         It return an integer.
+    Type: function
+    '''
+    print(len(iterable))
